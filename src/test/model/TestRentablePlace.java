2fef66903b138e5a710f7c2fded5de059102222e
simozmp <simo.zmp96@gmail.com>
2020-01-16 12:37:47 +0100

    Ant bug fix for Travis

--- src/test/model/TestRentablePlace.java
+++ src/test/model/TestRentablePlace.java
@@ -31,7 +31,7 @@ public class TestRentablePlace {
 	 */
 	public RentablePlace setup() {
 		
-		RentablePlace rentablePlace = new Hotel("nome", "indirizzo", "città", "Proprietario");
+		RentablePlace rentablePlace = new Hotel("nome", "indirizzo", "citta", "Proprietario");
 		
 		List<Person> people = new ArrayList<Person>();
 		

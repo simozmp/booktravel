89c383d94f38b6c78d4d41707f659992603b5a67
simozmp <simo.zmp96@gmail.com>
2020-02-03 18:23:17 +0100

    #31

---
+++ src/logic/model/Attraction.java
@@ -0,0 +1,13 @@
+package logic.model;
+
+import java.util.List;
+
+public class Attraction {
+	
+	private double itemId;
+	private String name;
+	private String description;
+	private List<String> tags;
+	private String location;
+
+}
